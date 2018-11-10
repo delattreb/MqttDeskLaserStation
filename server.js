3353efecda03980a43700de3f7c25b4e77e7bf7c
Bruno DELATTRE <delattreb@gmail.com>
2018-11-10 07:45:34 +0100

    Refactor to consume API

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ global.rootPath = __dirname
 
 let env = require(`${rootPath}/config/env`)
 
-let sql = require('./lib/sql')
 let mosca = require('./lib/mosca')
 
--- smartphone.js
+++ smartphone.js
@@ -4,7 +4,7 @@ let credential = require(`${rootPath}/config/credentials`)
 let env = require(`${rootPath}/config/env`)
 
 let clientMqtt = mqtt.connect(credential.address, env.mqttoptions)
-
+/*
 clientMqtt.publish(env.teamtopic, JSON.stringify({
     jacket: "5C:CF:7F:6B:45:C6",
     r: "100",
@@ -13,7 +13,7 @@ clientMqtt.publish(env.teamtopic, JSON.stringify({
     lum: "50",
     pseudo: "oneshoot",
     id: "12"
-}))
+}))*/
 //clientMqtt.publish(env.gametopic, JSON.stringify({ id: "12", target: "2" }))
 //clientMqtt.publish(env.partytopic, JSON.stringify({ poptime: "8", start: "9" }))
 //clientMqtt.publish(env.starttopic, JSON.stringify({ go: "1" }))
